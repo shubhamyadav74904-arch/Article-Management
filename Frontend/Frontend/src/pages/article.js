fbc13949cdf756f4a6347bdeabc9783b1d11d063
shubhamyadav74904-arch <shubhamyadav74904@gmail.com>
2026-07-31 19:06:41 +0530

    Update article.js

--- Frontend/Frontend/src/pages/article.js
+++ Frontend/Frontend/src/pages/article.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import articles from "./article-content";
 
-const url = process.env.REACT_APP_API_URL || '';
 
 const Article=()=>(
     <>
