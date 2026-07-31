1c4ec02acad471eb984ccbcc77091ae631532980
shubhamyadav74904-arch <shubhamyadav74904@gmail.com>
2026-07-31 19:01:59 +0530

    Fix article link path in ArticleList component

--- Frontend/Frontend/src/components/ArticleList.js
+++ Frontend/Frontend/src/components/ArticleList.js
@@ -8,8 +8,7 @@ const ArticleList=({articless})=>(
     {
     articless.map((article,key)=>(
         
-        <Link className="article-list-item" key = {key} to={`${url}/articles/${article.name}`}>
-          
+<Link className="article-list-item" key = {key} to={`/articles/${article.name}`}>          
             <h2>{article.name}</h2>
             <p>{article.content[0].substring(0,150)}</p>
         
